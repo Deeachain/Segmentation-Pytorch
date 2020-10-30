dd39b5dbd2420e51db5d84801a183918dbc813eb
Deeachain <Deeachain@163.com>
2020-10-30 19:41:37 +0800

    modify plot loss and miou

--- train.py
+++ train.py
@@ -317,52 +317,58 @@ def train_model(args):
         elif epoch % 10 == 0:
             torch.save(state, model_file_name)
 
+
+        f = open(args.savedir + 'log.txt', 'r')
+        next(f)
         # draw plots for visualization
-        if os.path.isfile(args.savedir + "loss.png"):
-            f = open(args.savedir + 'log.txt', 'r')
-            next(f)
-            epoch_list = []
+        if args.val_epochs == 1:
             lossTr_list = []
             lossVal_list = []
             for line in f.readlines():
-                epoch_list.append(float(line.strip().split()[0]))
                 lossTr_list.append(float(line.strip().split()[2]))
                 lossVal_list.append(float(line.strip().split()[3]))
-            assert len(epoch_list) == len(lossTr_list) == len(lossVal_list)
-
+            assert len(lossTr_list) == len(lossVal_list)
             fig1, ax1 = plt.subplots(figsize=(11, 8))
-
             ax1.plot(range(0, epoch + 1), lossTr_list, label='Train_loss')
             ax1.plot(range(0, epoch + 1), lossVal_list, label='Val_loss')
             ax1.set_title("Average training loss vs epochs")
             ax1.set_xlabel("Epochs")
             ax1.set_ylabel("Current loss")
             ax1.legend()
-
             plt.savefig(args.savedir + "loss.png")
             plt.close('all')
             plt.clf()
-        else:
-            fig1, ax1 = plt.subplots(figsize=(11, 8))
-
-            ax1.plot(range(0, epoch + 1), lossTr_list, label='Train_loss')
-            ax1.plot(range(0, epoch + 1), lossVal_list, label='Val_loss')
-            ax1.set_title("Average loss vs epochs")
-            ax1.set_xlabel("Epochs")
-            ax1.set_ylabel("Current loss")
-            ax1.legend()
-
-            plt.savefig(args.savedir + "loss.png")
-            plt.clf()
-
+            # plt Miou
             fig2, ax2 = plt.subplots(figsize=(11, 8))
-
-            ax2.plot(epoches, mIOU_val_list, label="Val IoU")
+            ax2.plot(range(0, epoch + 1), mIOU_val_list, label="Val IoU")
             ax2.set_title("Average IoU vs epochs")
             ax2.set_xlabel("Epochs")
             ax2.set_ylabel("Current IoU")
             ax2.legend()
-
+            plt.savefig(args.savedir + "mIou.png")
+            plt.close('all')
+        else:
+            fig1, ax1 = plt.subplots(figsize=(11, 8))
+            epoch_list = []
+            lossTr_list = []
+            for index, line in enumerate(f.readlines()):
+                lossTr_list.append(float(line.strip().split()[2]))
+                if index % args.val_epochs == 0:
+                    epoch_list.append(int(line.strip().split()[0]))
+            ax1.plot(range(0, epoch + 1), lossTr_list, label='Train_loss')
+            ax1.plot(epoch_list, lossVal_list, label='Val_loss')
+            ax1.set_title("Average loss vs epochs")
+            ax1.set_xlabel("Epochs")
+            ax1.set_ylabel("Current loss")
+            ax1.legend()
+            plt.savefig(args.savedir + "loss.png")
+            plt.clf()
+            fig2, ax2 = plt.subplots(figsize=(11, 8))
+            ax2.plot(epoch_list, mIOU_val_list, label="Val IoU")
+            ax2.set_title("Average IoU vs epochs")
+            ax2.set_xlabel("Epochs")
+            ax2.set_ylabel("Current IoU")
+            ax2.legend()
             plt.savefig(args.savedir + "mIou.png")
             plt.close('all')
 
@@ -393,10 +399,10 @@ def parse_args():
     # training hyper params
     parser.add_argument('--max_epochs', type=int, default=300,
                         help="the number of epochs: 300 for train set, 350 for train+val set")
-    parser.add_argument('--val_epochs', type=int, default=1,
+    parser.add_argument('--val_epochs', type=int, default=50,
                         help="the number of epochs: 100 for val set")
-    parser.add_argument('--random_mirror', type=bool, default=False, help="input image random mirror")
-    parser.add_argument('--random_scale', type=bool, default=False, help="input image resize 0.5 to 2")
+    parser.add_argument('--random_mirror', type=bool, default=True, help="input image random mirror")
+    parser.add_argument('--random_scale', type=bool, default=True, help="input image resize 0.5 to 2")
     parser.add_argument('--lr', type=float, default=5e-4, help="initial learning rate")
     parser.add_argument('--batch_size', type=int, default=8, help="the batch size is set to 16 for 2 GPUs")
     parser.add_argument('--optim', type=str.lower, default='adam', choices=['sgd', 'adam', 'radam', 'ranger'],
