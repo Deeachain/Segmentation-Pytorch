82da8187e0a078b5687332a95c7c8b7294467992
Deeachain <Deeachain@163.com>
2020-11-18 16:00:53 +0800

    modify val_epoch > 1 plot bug

--- utils/plot_log.py
+++ utils/plot_log.py
@@ -15,8 +15,8 @@ def draw_log(args, epoch, mIOU_val_list, lossVal_list):
         assert len(lossTr_list) == len(lossVal_list)
         # plt loss
         fig1, ax1 = plt.subplots(figsize=(11, 8))
-        ax1.plot(range(0, epoch + 1), lossTr_list, label='Train_loss')
-        ax1.plot(range(0, epoch + 1), lossVal_list, label='Val_loss')
+        ax1.plot(range(1, epoch + 1), lossTr_list, label='Train_loss')
+        ax1.plot(range(1, epoch + 1), lossVal_list, label='Val_loss')
         ax1.set_title("Average training loss vs epochs")
         ax1.set_xlabel("Epochs")
         ax1.set_ylabel("Current loss")
@@ -26,7 +26,7 @@ def draw_log(args, epoch, mIOU_val_list, lossVal_list):
         plt.clf()
         # plt Miou
         fig2, ax2 = plt.subplots(figsize=(11, 8))
-        ax2.plot(range(0, epoch + 1), mIOU_val_list, label="Val IoU")
+        ax2.plot(range(1, epoch + 1), mIOU_val_list, label="Val IoU")
         ax2.set_title("Average IoU vs epochs")
         ax2.set_xlabel("Epochs")
         ax2.set_ylabel("Current IoU")
@@ -40,9 +40,9 @@ def draw_log(args, epoch, mIOU_val_list, lossVal_list):
         lossTr_list = []
         for index, line in enumerate(f.readlines()):
             lossTr_list.append(float(line.strip().split()[2]))
-            if index % args.val_epochs == 0:
+            if (index + 1) / args.val_epochs == 0 or index == 0:
                 epoch_list.append(int(line.strip().split()[0]))
-        ax1.plot(range(0, epoch + 1), lossTr_list, label='Train_loss')
+        ax1.plot(range(1, epoch + 1), lossTr_list, label='Train_loss')
         ax1.plot(epoch_list, lossVal_list, label='Val_loss')
         ax1.set_title("Average loss vs epochs")
         ax1.set_xlabel("Epochs")
