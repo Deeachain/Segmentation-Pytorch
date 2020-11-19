52713f115de7fd2fe2320644ebeac8541f99b2e4
Deeachain <Deeachain@163.com>
2020-11-19 14:46:19 +0800

    optimize log display

--- utils/record_log.py
+++ utils/record_log.py
@@ -1,4 +1,4 @@
-import os
+from prettytable import PrettyTable
 
 class record_log():
     def __init__(self, args):
@@ -12,21 +12,32 @@ class record_log():
 
     def initial_logfile(self):
         logFileLoc = self.args.savedir + self.args.logFile
-        if os.path.isfile(logFileLoc):
-            logger = open(logFileLoc, 'a')
-        else:
-            logger = open(logFileLoc, 'w')
-            logger.write(
-                "%s\t%s\t\t%s\t%s\t%s\t%s\n" % ('Epoch', '   lr', 'Loss(Tr)', 'Loss(Val)', 'FWIOU(Val)', 'mIOU(Val)'))
+        logger = open(logFileLoc, 'w')
+        logger.write(
+            "%s\t%s\t\t%s\t%s\t%s\t%s\n" % ('Epoch', '   lr', 'Loss(Tr)', 'Loss(Val)', 'FWIOU(Val)', 'mIOU(Val)'))
         return logger
 
-    def record_trainVal_log(self, logger, epoch, lr, lossTr, val_loss, FWIoU, mIOU_val, per_class_iu):
+    def resume_logfile(self):
+        logFileLoc = self.args.savedir + self.args.logFile
+        logger_recored = open(logFileLoc, 'r')
+        next(logger_recored)
+        lines = logger_recored.readlines()
+        logger_recored.close()
+        logger = open(logFileLoc, 'a+')
+        return logger, lines
+
+    def record_trainVal_log(self, logger, epoch, lr, lossTr, val_loss, FWIoU, mIOU_val, PerMiou_set, class_dict_df):
         logger.write(
             "%d\t%.6f\t%.4f\t\t%.4f\t\t%0.4f\t\t%0.4f\t\t%s\n" % (
-                epoch, lr, lossTr, val_loss, FWIoU, mIOU_val, per_class_iu))
+                epoch, lr, lossTr, val_loss, FWIoU, mIOU_val, PerMiou_set))
         logger.flush()
-        print("Epoch  %d\tlr= %.6f\tTrain Loss = %.4f\tVal Loss = %.4f\tFWIOU(val) = %.4f\tmIOU(val) = %.4f\tper_class_iu= %s\n" \
-              % (epoch, lr, lossTr, val_loss, FWIoU, mIOU_val, str(per_class_iu)))
+        print("Epoch %d\tlr= %.6f\tTrain Loss = %.4f\tVal Loss = %.4f\tFWIOU(val) = %.4f" \
+              % (epoch, lr, lossTr, val_loss, FWIoU))
+
+        t = PrettyTable(['label_index', 'class_name', 'class_iou'])
+        for index in range(class_dict_df.shape[0]):
+            t.add_row([class_dict_df['label_index'][index], class_dict_df['class_name'][index], PerMiou_set[index]])
+        print(t.get_string(title="Miou is {}".format(mIOU_val)))
 
     def record_train_log(self, logger, epoch, lr, lossTr):
         logger.write("%d\t%.6f\t%.4f\n" % (epoch, lr, lossTr))
