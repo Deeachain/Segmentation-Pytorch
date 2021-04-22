718fd7e609fec308eb2bb15d4fde0587062b565a
Deechain <Deeachain@163.com>
2021-04-22 21:35:45 +0800

    V1 commit

--- dataset/generate_txt.py
+++ dataset/generate_txt.py
@@ -1,23 +1,36 @@
+# _*_ coding: utf-8 _*_
+"""
+Time:     2020/11/30 下午5:02
+Author:   Ding Cheng(Deeachain)
+File:     generate_txt.py
+Describe: Write during my study in Nanjing University of Information and Secience Technology
+Github:   https://github.com/Deeachain
+"""
 import glob
 import random
+import os
 
-# isprs 6类数据集
-def generate_txt(mode = 'train'):
-    filename_list = glob.glob('/data/dingcheng/ISPRS/postdam/val/1024_image/*.tif')
-    filename_list.sort()
-    # print(filename_list)
 
-    random.shuffle(filename_list)
+def generate_txt(mode='train'):
+    root_dir = 'data/open_data/cityscapes/leftImg8bit/'
+    dir_list = os.listdir(os.path.join(root_dir, mode))
+    filename_list = []
+    for dir in dir_list:
+        filename = glob.glob(os.path.join(root_dir, mode, dir) + '/'+'*.png')
+        filename.sort()
 
-    with open('isprs_{}_list.txt'.format(mode), 'w+') as f:
+        random.shuffle(filename)
+        filename_list.extend(filename)
+    with open('cityscapes_{}_list.txt'.format(mode), 'w+') as f:
         for filename in filename_list[:]:
-            filename_gt = filename.replace('1024_image', '1024_label')
-            # print(filename, filename_gt)
-            f.write('{}/{}/{}\t{}/{}/{}\n'.format(mode, filename.split('/')[-2], filename.split('/')[-1],
-                                                  mode,filename_gt.split('/')[-2], filename_gt.split('/')[-1]))
+            filename_gt = filename.replace('leftImg8bit', 'gtFine')
+            print(filename, filename_gt)
+            f.write('{}/{}/{}/{}\t{}/{}/{}/{}\n'.format(filename.split('/')[-4], filename.split('/')[-3],
+                                                        filename.split('/')[-2], filename.split('/')[-1],
+                                                        filename_gt.split('/')[-4], filename_gt.split('/')[-3],
+                                                        filename_gt.split('/')[-2], filename_gt.split('/')[-1]))
 
 
 if __name__ == '__main__':
-    # generate_txt('train')
-    generate_txt('test')
+    generate_txt('train')
     print('Finsh!')
