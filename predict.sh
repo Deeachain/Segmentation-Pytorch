a019a9f828916a4b0d66487dbc1026200afdd1e9
Deechain <Deeachain@163.com>
2021-04-24 08:12:33 +0800

    V1 commit

--- predict.sh
+++ predict.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-python predict.py --model DDRNet \
-                  --checkpoint /data/dingcheng/segment/checkpoint/cityscapes/DDRNet-75.24/best_model.pth \
+python predict.py --model PSPNet_res50 \
+                  --checkpoint /data/dingcheng/segment/checkpoint/cityscapes/PSPNet_res50/best_model.pth \
                   --out_stride 8 \
                   --root '/data/open_data' \
                   --dataset cityscapes \
                   --predict_type validation \
                   --predict_mode whole \
-                  --crop_size 1024 \
-                  --tile_hw_size '1024,1024' \
+                  --crop_size 768 \
+                  --tile_hw_size '768,768' \
                   --batch_size 2 \
                   --gpus 3 \
                   --overlap 0.3 \
