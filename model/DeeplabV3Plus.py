fb84d482caf116f799c27de3d57a81173e9d95e4
Deechain <Deeachain@163.com>
2021-05-12 19:52:27 +0800

    V1 commit

--- model/DeeplabV3Plus.py
+++ model/DeeplabV3Plus.py
@@ -1,8 +1,10 @@
 # _*_ coding: utf-8 _*_
 """
-Time:     2021/3/13 12:37
-Author:   Ding Cheng(Deeachain)
-File:     DeeplabV3Plus.py
+Time:     2020/11/28 下午12:32
+Author:   Cheng Ding(Deeachain)
+Version:  V 0.1
+File:     DeeplabV3+.py
+Describe: Write during my study in Nanjing University of Information and Secience Technology
 Github:   https://github.com/Deeachain
 """
 import math
@@ -11,8 +13,56 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.model_zoo as model_zoo
 from model.sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
+from torchsummary import summary
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
 
 BatchNorm2d = SynchronizedBatchNorm2d
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -21,8 +71,8 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               dilation=dilation, padding=dilation, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, dilation=dilation, padding=dilation,
+                               bias=False)
         self.bn2 = BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.bn3 = BatchNorm2d(planes * 4)
@@ -33,7 +83,6 @@ class Bottleneck(nn.Module):
 
     def forward(self, x):
         residual = x
-
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -53,9 +102,9 @@ class Bottleneck(nn.Module):
 
         return out
 
-class ResNet(nn.Module):
 
-    def __init__(self, nInputChannels, block, layers, os=16, pretrained=False):
+class ResNet(nn.Module):
+    def __init__(self, block, layers, os=16):
         self.inplanes = 64
         super(ResNet, self).__init__()
         if os == 16:
@@ -68,10 +117,8 @@ class ResNet(nn.Module):
             blocks = [1, 2, 1]
         else:
             raise NotImplementedError
-
         # Modules
-        self.conv1 = nn.Conv2d(nInputChannels, 64, kernel_size=7, stride=2, padding=3,
-                                bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
@@ -80,18 +127,13 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, layers[1], stride=strides[1], dilation=dilations[1])
         self.layer3 = self._make_layer(block, 256, layers[2], stride=strides[2], dilation=dilations[2])
         self.layer4 = self._make_MG_unit(block, 512, blocks=blocks, stride=strides[3], dilation=dilations[3])
-
         self._init_weight()
-
-        if pretrained:
-            self._load_pretrained_model()
 
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
                 BatchNorm2d(planes * block.expansion),
             )
 
@@ -113,10 +155,10 @@ class ResNet(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, dilation=blocks[0]*dilation, downsample=downsample))
+        layers.append(block(self.inplanes, planes, stride, dilation=blocks[0] * dilation, downsample=downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, len(blocks)):
-            layers.append(block(self.inplanes, planes, stride=1, dilation=blocks[i]*dilation))
+            layers.append(block(self.inplanes, planes, stride=1, dilation=blocks[i] * dilation))
 
         return nn.Sequential(*layers)
 
@@ -125,7 +167,6 @@ class ResNet(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-
         x = self.layer1(x)
         low_level_feat = x
         x = self.layer2(x)
@@ -142,20 +183,6 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _load_pretrained_model(self):
-        pretrain_dict = model_zoo.load_url('https://download.pytorch.org/models/resnet101-5d3b4d8f.pth')
-        model_dict = {}
-        state_dict = self.state_dict()
-        for k, v in pretrain_dict.items():
-            if k in state_dict:
-                model_dict[k] = v
-        state_dict.update(model_dict)
-        self.load_state_dict(state_dict)
-
-def ResNet101(nInputChannels=3, os=16, pretrained=False):
-    model = ResNet(nInputChannels, Bottleneck, [3, 4, 23, 3], os, pretrained=pretrained)
-    return model
-
 
 class ASPP_module(nn.Module):
     def __init__(self, inplanes, planes, dilation):
@@ -170,13 +197,11 @@ class ASPP_module(nn.Module):
                                             stride=1, padding=padding, dilation=dilation, bias=False)
         self.bn = BatchNorm2d(planes)
         self.relu = nn.ReLU()
-
         self._init_weight()
 
     def forward(self, x):
         x = self.atrous_convolution(x)
         x = self.bn(x)
-
         return self.relu(x)
 
     def _init_weight(self):
@@ -189,14 +214,12 @@ class ASPP_module(nn.Module):
                 m.bias.data.zero_()
 
 
-class DeepLabv3_plus(nn.Module):
-    def __init__(self, nInputChannels=3, n_classes=21, os=16, pretrained=False, freeze_bn=False):
-        super(DeepLabv3_plus, self).__init__()
+class Deeplabv3plus(nn.Module):
+    def __init__(self, backbone, num_classes=3, os=16, freeze_bn=False):
+        super(Deeplabv3plus, self).__init__()
 
-        # Atrous Conv
-        self.resnet_features = ResNet101(nInputChannels, os, pretrained=pretrained)
+        self.resnet_features = backbone
 
-        # ASPP
         if os == 16:
             dilations = [1, 6, 12, 18]
         elif os == 8:
@@ -208,7 +231,6 @@ class DeepLabv3_plus(nn.Module):
         self.aspp2 = ASPP_module(2048, 256, dilation=dilations[1])
         self.aspp3 = ASPP_module(2048, 256, dilation=dilations[2])
         self.aspp4 = ASPP_module(2048, 256, dilation=dilations[3])
-
         self.relu = nn.ReLU()
 
         self.global_avg_pool = nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)),
@@ -219,17 +241,16 @@ class DeepLabv3_plus(nn.Module):
         self.conv1 = nn.Conv2d(1280, 256, 1, bias=False)
         self.bn1 = BatchNorm2d(256)
 
-        # adopt [1x1, 48] for channel reduction.
         self.conv2 = nn.Conv2d(256, 48, 1, bias=False)
         self.bn2 = BatchNorm2d(48)
-
         self.last_conv = nn.Sequential(nn.Conv2d(304, 256, kernel_size=3, stride=1, padding=1, bias=False),
                                        BatchNorm2d(256),
                                        nn.ReLU(),
                                        nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
                                        BatchNorm2d(256),
                                        nn.ReLU(),
-                                       nn.Conv2d(256, n_classes, kernel_size=1, stride=1))
+                                       nn.Conv2d(256, num_classes, kernel_size=1, stride=1))
+
         if freeze_bn:
             self._freeze_bn()
 
@@ -240,25 +261,21 @@ class DeepLabv3_plus(nn.Module):
         x3 = self.aspp3(x)
         x4 = self.aspp4(x)
         x5 = self.global_avg_pool(x)
-        x5 = F.upsample(x5, size=x4.size()[2:], mode='bilinear', align_corners=True)
-
+        x5 = F.interpolate(x5, size=x4.size()[2:], mode='bilinear', align_corners=True)
         x = torch.cat((x1, x2, x3, x4, x5), dim=1)
-
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = F.upsample(x, size=(int(math.ceil(input.size()[-2]/4)),
-                                int(math.ceil(input.size()[-1]/4))), mode='bilinear', align_corners=True)
+        x = F.interpolate(x, size=(int(math.ceil(input.size()[-2] / 4)),
+                                   int(math.ceil(input.size()[-1] / 4))), mode='bilinear', align_corners=True)
 
         low_level_features = self.conv2(low_level_features)
         low_level_features = self.bn2(low_level_features)
         low_level_features = self.relu(low_level_features)
 
-
         x = torch.cat((x, low_level_features), dim=1)
         x = self.last_conv(x)
         x = F.interpolate(x, size=input.size()[2:], mode='bilinear', align_corners=True)
-
         return x
 
     def _freeze_bn(self):
@@ -275,36 +292,53 @@ class DeepLabv3_plus(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def get_1x_lr_params(self):
-        """
-        This generator returns all the parameters of the net except for
-        the last classification layer. Note that for each batchnorm layer,
-        requires_grad is set to False in deeplab_resnet.py, therefore this function does not return
-        any batchnorm parameter
-        """
-        b = [self.resnet_features]
-        for i in range(len(b)):
-            for k in b[i].parameters():
-                if k.requires_grad:
-                    yield k
+
+# def Deeplabv3plus_res18(num_classes=3, os=16, pretrained=False):
+#     resnet50 = ResNet(BasicBlock, [2, 2, 2, 2], os)
+#     if pretrained:
+#         pretrain_dict = model_zoo.load_url(model_urls['resnet18'])
+#         model_dict = {}
+#         state_dict = resnet50.state_dict()
+#         for k, v in pretrain_dict.items():
+#             if k in state_dict:
+#                 model_dict[k] = v
+#         state_dict.update(model_dict)
+#         resnet50.load_state_dict(state_dict)
+#     model = Deeplabv3plus(backbone=resnet50, num_classes=num_classes, os=os, freeze_bn=False)
+#     return model
 
 
-    def get_10x_lr_params(self):
-        """
-        This generator returns all the parameters for the last layer of the net,
-        which does the classification of pixel into classes
-        """
-        b = [self.aspp1, self.aspp2, self.aspp3, self.aspp4, self.conv1, self.conv2, self.last_conv]
-        for j in range(len(b)):
-            for k in b[j].parameters():
-                if k.requires_grad:
-                    yield k
+def Deeplabv3plus_res50(num_classes=3, os=16, pretrained=False):
+    resnet50 = ResNet(Bottleneck, [3, 4, 6, 3], os)
+    if pretrained:
+        pretrain_dict = model_zoo.load_url(model_urls['resnet50'])
+        model_dict = {}
+        state_dict = resnet50.state_dict()
+        for k, v in pretrain_dict.items():
+            if k in state_dict:
+                model_dict[k] = v
+        state_dict.update(model_dict)
+        resnet50.load_state_dict(state_dict)
+    model = Deeplabv3plus(backbone=resnet50, num_classes=num_classes, os=os, freeze_bn=False)
+    return model
 
 
-if __name__ == "__main__":
-    model = DeepLabv3_plus(nInputChannels=3, n_classes=21, os=16, pretrained=True)
-    model.eval()
-    image = torch.randn(1, 3, 512, 512)
-    with torch.no_grad():
-        output = model.forward(image)
-    print(output.size())
+def Deeplabv3plus_res101(num_classes=3, os=16, pretrained=False):
+    resnet101 = ResNet(Bottleneck, [3, 4, 23, 3], os)
+    if pretrained:
+        pretrain_dict = model_zoo.load_url(model_urls['resnet101'])
+        model_dict = {}
+        state_dict = resnet101.state_dict()
+        for k, v in pretrain_dict.items():
+            if k in state_dict:
+                model_dict[k] = v
+        state_dict.update(model_dict)
+        resnet101.load_state_dict(state_dict)
+    model = Deeplabv3plus(backbone=resnet101, num_classes=num_classes, os=os, freeze_bn=False)
+    return model
+
+
+"""print layers and params of network"""
+if __name__ == '__main__':
+    model = Deeplabv3plus_res50(num_classes=3, os=16, pretrained=False)
+    summary(model, (3, 512, 512), device="cpu")
